63425a5134c50de9fee25bda4f913984c59b8aa2
Elviss Strazdins <elviss@elviss.lv>
2021-03-02 20:52:42 +0200

    Remove the unneeded packages

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,4 @@
-import React, { useState } from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams
-} from "react-router-dom";
+import React from "react";
 import { Admin, Resource, List, Datagrid, TextField, EmailField } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
@@ -26,49 +19,12 @@ const UserList = props => (
 );
 
 export function App() {
-    const [state, setState] = useState("Load");
-
     const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
     //const dataProvider = jsonServerProvider('/api');
 
     return <div className="App">
         <Admin dataProvider={dataProvider}>
             <Resource name="users" list={UserList} />
-            <Resource name="bricks" list={UserList} />
         </Admin>
-
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/tables">Tables</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Switch>
-                    <Route path="/tables">
-                        <Tables />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-
-        <button onClick={() => setState("Loading")}>{state}</button>
     </div>;
-}
-
-function Home() {
-    return <h2>Home</h2>;
-}
-
-function Tables() {
-    return <h2>Tables</h2>;
 }
